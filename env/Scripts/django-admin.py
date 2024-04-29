5b29e45442d4228818f768771902ca6c1ba59382
Cactuskiller <daniaayad728@gamil.com>
2024-04-29 18:08:15 +0300

    python version changed!

--- env/Scripts/django-admin.py
+++ env/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\tariq\OneDrive\Documents\Code\Python\dania\CarZone-gitproject\env\Scripts\python.exe
+#!C:\Users\HP-pc\Documents\Project\CarZone-gitproject\env\Scripts\python.exe
 from django.core import management
 
 if __name__ == "__main__":
